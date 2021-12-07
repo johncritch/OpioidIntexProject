0f58bfa565514a0c0173141fabd4673469d55b06
johncritch <johnjohncritchcritch@gmail.com>
2021-12-06 17:48:59 -0700

    changed db

--- OpioidIntexProject/settings.py
+++ OpioidIntexProject/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
@@ -85,9 +86,9 @@ DATABASES = {
 	'default': {
     	'ENGINE': 'django.db.backends.postgresql',
     	'NAME': 'intex31', 
-    	'USER': 'admin31',
-    	'PASSWORD': 'password31!',
-    	'HOST': 'intext31j.postgres.database.azure.com',
+    	'USER': 'postgres',
+    	'PASSWORD': 'jcritchlow',
+    	'HOST': 'localhost',
 	}
 }
 db_from_env = dj_database_url.config(conn_max_age=600)
